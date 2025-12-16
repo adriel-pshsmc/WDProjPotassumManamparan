c9d603e8902c85388c6d2919f5bcbd0056865889
adriel-pshsmc <b2029alcmanamparan@pshs.edu.ph>
2025-12-16 11:23:35 +0800

    adriel 11:25 12/16/25

---
+++ project_script.js
@@ -0,0 +1,15 @@
+function home() {
+     document.getElementById("selected").innerHTML = '<iframe class="content" src="/subpages/home.html"></iframe>';
+}
+function documents() {
+     document.getElementById("selected").innerHTML = '<iframe class="content" src="/subpages/documents.html"></iframe>';
+}
+function events() {
+     document.getElementById("selected").innerHTML = '<iframe class="content" src="/subpages/events.html"></iframe>';
+}
+function login() {
+     document.getElementById("selected").innerHTML = '<iframe class="content" src="/subpages/login.html"></iframe>';
+}
+function references() {
+     document.getElementById("selected").innerHTML = '<iframe class="content" src="/subpages/references.html"></iframe>';
+}
