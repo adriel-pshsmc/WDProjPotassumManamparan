88801a20aa8574901ddf4f6e7006acd8b9c6f25f
adriel-pshsmc <b2029alcmanamparan@pshs.edu.ph>
2026-01-05 18:16:55 +0800

    adriel 01/05/26 6:16 github compatibility update

--- project_script.js
+++ project_script.js
@@ -1,27 +1,27 @@
 function home() {
-     document.getElementById("selected").innerHTML = '<iframe class="content" src="/subpages/home.html"></iframe>';
+     document.getElementById("selected").innerHTML = '<iframe class="content" src="subpages/home.html"></iframe>';
 }
 function documents() {
-     document.getElementById("selected").innerHTML = '<iframe class="content" src="/subpages/documents.html"></iframe>';
+     document.getElementById("selected").innerHTML = '<iframe class="content" src="subpages/documents.html"></iframe>';
 }
 function events() {
-     document.getElementById("selected").innerHTML = '<iframe class="content" src="/subpages/events.html"></iframe>';
+     document.getElementById("selected").innerHTML = '<iframe class="content" src="subpages/events.html"></iframe>';
 }
 function login() {
-     document.getElementById("selected").innerHTML = '<iframe class="content" src="/subpages/login.html"></iframe>';
+     document.getElementById("selected").innerHTML = '<iframe class="content" src="subpages/login.html"></iframe>';
 }
 function references() {
-     document.getElementById("selected").innerHTML = '<iframe class="content" src="/subpages/references.html"></iframe>';
+     document.getElementById("selected").innerHTML = '<iframe class="content" src="subpages/references.html"></iframe>';
 }
 function Alab() {
-     document.getElementById("selected").innerHTML = '<iframe class="content" src="/subpages/events-subpages/alab.html"></iframe>';
+     document.getElementById("selected").innerHTML = '<iframe class="content" src="subpages/events-subpages/alab.html"></iframe>';
 }
 function Familyday() {
-     document.getElementById("selected").innerHTML = '<iframe class="content" src="/subpages/events-subpages/familyday.html"></iframe>';
+     document.getElementById("selected").innerHTML = '<iframe class="content" src="subpages/events-subpages/familyday.html"></iframe>';
 }
 function Midyearfair() {
-     document.getElementById("selected").innerHTML = '<iframe class="content" src="/subpages/events-subpages/midyearfair.html"></iframe>';
+     document.getElementById("selected").innerHTML = '<iframe class="content" src="subpages/events-subpages/midyearfair.html"></iframe>';
 }
 function Intramurals() {
-     document.getElementById("selected").innerHTML = '<iframe class="content" src="/subpages/events-subpages/intramurals.html"></iframe>';
+     document.getElementById("selected").innerHTML = '<iframe class="content" src="subpages/events-subpages/intramurals.html"></iframe>';
 }
